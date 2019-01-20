66c9c952354d17e22a3ce741d47748b3e59e8fb5
zhcui <zcui@caltech.edu>
2019-01-20 13:08:05 -0800

    atomic scf and tile basis

--- atomic_calc.py
+++ atomic_calc.py
@@ -10,8 +10,9 @@ import copy
 import numpy as np
 import scipy.linalg as la
 
-from pyscf import lo, tools
+from pyscf import lo, tools, lib
 from pyscf.pbc import gto, scf, df
+from pyscf.lib import logger as log
 from pyscf.tools import molden
 
 def atom_spin(name):
@@ -44,25 +45,47 @@ def atom_norb(name, ref_basis='minao'):
     """
     Given atom name, return number of orb used for basis.
     """
-    # FIXME PP treatment.
+    # FIXME PP treatment, remove the warning
     assert(ref_basis in ['minao', 'gth-szv'])
     if ref_basis == 'gth-szv':
         pseudo = 'gth-pade'
     else:
         pseudo = None
     pmol          = gto.Cell()
-    pmol.verbose  = 1
+    pmol.verbose  = 0
     pmol.atom     = [[name, (0.0, 0.0, 0.0)]]
     pmol.a        = np.eye(3) * 5.0
     pmol.basis    = ref_basis
     pmol.pseudo   = pseudo
+    pmol.spin     = atom_spin(name)
     pmol.build()
     return pmol.nao_nr(), pmol.ao_labels()    
 
 class Atom_Info(object):
+    """
+    Atomic information.
+    mol             :   input mol object
+    names           :   atom names
+    pos             :   position of each atom
+    names_uniq      :   unique atom names (species)
+    idx_inv         :   idx to reconstruct names from names_uniq
+    atom_num        :   number of atoms of each species
+    spins           :   spin of each species
+    cores           :   core of each species
+    norbs           :   number of minimal orbitals of each species
+    ao_labels       :   corresponding orbital labels
+    basis           :   basis, dict, use atomic name as key
+    pseufo          :   pseudo, dict, use atomic name as key
+    basis_idx_old   :   basis idx in original mol/cell, dict, use atomic name as key
+    basis_idx_new   :   basis idx in new basis, dict, use atomic name as key
+    mfs             :   mf for atomic calculation
+    """
     def __init__(self, mol, **kwargs):
+        self.mol = mol
         self.names, self.pos= zip(*mol._atom)
-        self.names_uniq, self.idx_inv = np.unique(self.names, return_inverse=True)
+        self.names_uniq, self.idx_inv, self.atom_num = \
+                np.unique(self.names, return_inverse=True, return_counts=True)
+        self.name_dict = dict(zip(self.names_uniq, range(len(self.names_uniq))))
         self.spins = [atom_spin(name) for name in self.names_uniq]
         self.cores = [atom_core(name) for name in self.names_uniq] # FIXME core info
         self.norbs = []
@@ -77,13 +100,30 @@ class Atom_Info(object):
             self.ao_labels.append(ao_labels)
         self.basis = mol._basis
         self.pseudo = mol._pseudo
-        self.basis_partition = {}
+        self.basis_idx_old = {}
+        self.basis_idx_new = {}
+        start = 0
         for i, (sh0, sh1, ao0, ao1) in enumerate(mol.offset_nr_by_atom()):
             name = self.names[i]
-            if name not in self.basis_partition:
-                self.basis_partition[name] = [range(ao0, ao1)]
+            idx = self.name2idx(name)
+            norb = self.norbs[idx]
+            if name not in self.basis_idx_old:
+                self.basis_idx_old[name] = [range(ao0, ao1)]
             else:
-                self.basis_partition[name].append(range(ao0, ao1))
+                self.basis_idx_old[name].append(range(ao0, ao1))
+            if name not in self.basis_idx_new:
+                self.basis_idx_new[name] = [range(start, start+norb)]
+            else:
+                self.basis_idx_new[name].append(range(start, start+norb))
+            start += norb
+        self.mfs = [] # to be calculate
+
+    def nao_nr(self):
+        return np.sum([self.norbs[i] * self.atom_num[i] \
+                for i, name in enumerate(self.names_uniq)])
+
+    def name2idx(self, name):
+        return self.name_dict[name]
 
 def build_atom_cell(name, basis, pseudo, spin, charge=0, box_length=10.0):
     """
@@ -108,23 +148,53 @@ def atom_scf(mol, method='ROHF', **kwargs):
         basis = {name: info.basis[name]}
         pseudo = {name: info.pseudo[name]}
         spin = info.spins[i]
+        log.debug(mol, "\natom calculation")
+        log.debug(mol, 'atom = %s', name)
+        log.debug(mol, 'spin = %s', spin)
+        log.debug(mol, 'basis = \n%s', basis)
+        log.debug(mol, 'pseudo = \n%s', pseudo)
+        
         pmol = build_atom_cell(name, basis, pseudo, spin)
-        pmol.verbose = 1
-        print "name: ", name
-        print "spin: ", spin
+        pmol.verbose = 3
 
         pmf = scf.KROHF(pmol)
         pmf.max_cycle  = 5000
         pmf.conv_tol   = 1e-6
         pmf = pmf.density_fit()
         pmf.scf()
-        
-        nocc = np.sum(pmf.mo_occ > 1e-12)
-        assert(nocc <= info.norbs[i])
+        info.mfs.append(pmf)
 
-        print pmf.mo_occ
-        print info.norbs[i]
-        print pmf.mo_coeff
+        nocc = np.sum(np.asarray(pmf.mo_occ) > 0.0)
+        assert(nocc <= info.norbs[i])
+        log.debug(mol, "mo_occ: %s", pmf.mo_occ)
+        log.debug(mol, "nocc: %s", nocc)
+        log.debug(mol, "norbs: %s", info.norbs[i])
+        log.debug(mol, "ao label \n %s", info.ao_labels[i])
+    return info
+
+def tile_ao_basis(info):
+    """
+    Tile new calculated minimal AO basis to form a basis expanded in 
+    Cell's large basis.
+    """
+    assert(info.mfs)
+    mo_coeff_B2 = [mf.mo_coeff[0][:, :info.norbs[i]] \
+            for i, mf in enumerate(info.mfs)]
+    nbas_B2 = info.nao_nr()
+    nbas_B1 = info.mol.nao_nr()
+    B2 = np.zeros((nbas_B1, nbas_B2))
+    for i, name in enumerate(info.names_uniq):
+        #idx = info.name2idx(name)
+        bas_idx_old = info.basis_idx_old[name]
+        bas_idx_new = info.basis_idx_new[name]
+        assert(len(bas_idx_old) == len(bas_idx_new))
+        for j in range(len(bas_idx_old)):
+            B2[np.ix_(bas_idx_old[j], bas_idx_new[j])] = \
+                    mo_coeff_B2[i]
+    
+    # TODO get overlap
+    print B2.T.dot(B2)
+
 
 ### --------- test functions ---------- ###
 
@@ -147,7 +217,7 @@ def build_diamond_cell():
     #cell.basis = '6311g'
     cell.basis = 'gth-dzv'
     cell.pseudo = 'gth-pade'
-    cell.verbose = 4
+    cell.verbose = 5
     cell.build(unit='Angstrom')
     return cell
 
@@ -169,12 +239,34 @@ def build_random_cell():
     cell.ke_cutoff = 100
     cell.basis = 'gth-dzv'
     cell.pseudo = 'gth-pade'
-    cell.verbose = 4
+    cell.verbose = 5
+    cell.spin = 1
+    cell.build(unit='Angstrom')
+    return cell
+
+def build_simple_cell():
+    cell = gto.Cell()
+    cell.a = '''
+    3.5668  0       0
+    0       4.5668  0
+    0       0       3.5668'''
+    cell.atom='''C 0.      0.      0.
+       H 0.8917  0.8917  0.8917
+       H 1.7834  1.7834  0. '''
+
+    cell.ke_cutoff = 100
+    cell.basis = 'gth-dzv'
+    cell.pseudo = 'gth-pade'
+    cell.verbose = 5
+    cell.spin = 1
     cell.build(unit='Angstrom')
     return cell
 
 if __name__ == '__main__':
+    np.set_printoptions(3, linewidth=1000)
     #cell = build_diamond_cell()
-    cell = build_random_cell()
+    #cell = build_random_cell()
+    cell = build_simple_cell()
     #atom_info = Atom_Info(cell, ref_basis='gth-szv')
-    atom_scf(cell, ref_basis='gth-szv')
+    info = atom_scf(cell, ref_basis='gth-szv')
+    tile_ao_basis(info)
